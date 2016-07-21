d15b016812fdc399c51eab4d10addba218a991cb
Austry <iliazasimov@gmail.com>
2016-07-22 00:15:20 +0300

    Animation

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -1,18 +1,31 @@
 package ru.yandex.yamblz.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import ru.yandex.yamblz.R;
+import ru.yandex.yamblz.ui.touch.AnimationTouchListener;
 
 public class ContentFragment extends BaseFragment {
+    private final String LOG_TAG = this.getClass().getName();
+
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View result = inflater.inflate(R.layout.fragment_content, container, false);
+        View toDrag = result.findViewById(R.id.toDrag);
+        View deleteButton = result.findViewById(R.id.btnDelete);
+
+        toDrag.setOnTouchListener(
+                new AnimationTouchListener((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE), deleteButton)
+        );
+
+        return result;
     }
 }
