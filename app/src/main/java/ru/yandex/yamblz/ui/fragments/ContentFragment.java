9888127236a52c526efa3ee8cf3254876b2b5411
Valentin Postnov <valpostnov@gmail.com>
2016-07-22 23:58:05 +0300

    hw done

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -7,12 +7,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import ru.yandex.yamblz.R;
+import ru.yandex.yamblz.ui.views.SwipeFrameLayout;
 
-public class ContentFragment extends BaseFragment {
+public class ContentFragment extends BaseFragment
+{
+    private Unbinder viewBinder;
+
+    @BindView(R.id.swipe_view) View swipeView;
+    @BindView(R.id.delete_view) View deleteView;
+    @BindView(R.id.main_layout) SwipeFrameLayout swipeLayout;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
     @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle bundle)
+    {
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        viewBinder = ButterKnife.bind(this, view);
+        swipeLayout.addSwipeView(swipeView);
+        swipeLayout.addDeleteView(deleteView);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        if (viewBinder != null)
+        {
+            viewBinder.unbind();
+        }
+        super.onDestroyView();
     }
 }
