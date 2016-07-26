d252410c0c9d91c8b239e9b3b84dd47bd4bbeb5f
Timofey Talenfeld <taymon@yandex.ru>
2016-07-26 13:02:21 +0300

    make HW

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -6,13 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import ru.yandex.yamblz.R;
 
 public class ContentFragment extends BaseFragment {
+
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_content, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
