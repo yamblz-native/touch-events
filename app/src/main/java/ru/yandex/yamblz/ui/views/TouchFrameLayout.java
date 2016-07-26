9412a3423a7e945ea221b9b83008eec7210591fe
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2016-07-27 00:23:19 +0300

    Separate 'dismiss' and 'scroll' actions; handle Fling

--- app/src/main/java/ru/yandex/yamblz/ui/views/TouchFrameLayout.java
+++ app/src/main/java/ru/yandex/yamblz/ui/views/TouchFrameLayout.java
@@ -7,26 +7,42 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Scroller;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import ru.yandex.yamblz.R;
+
+import static ru.yandex.yamblz.ui.views.TouchFrameLayout.Action.DISMISS;
+import static ru.yandex.yamblz.ui.views.TouchFrameLayout.Action.NONE;
+import static ru.yandex.yamblz.ui.views.TouchFrameLayout.Action.SCROLL;
 
 public class TouchFrameLayout extends FrameLayout {
     private GestureDetector detector;
-    private View dismissible;
+    private Scroller scroller;
+    private Action action;
+    private int maxScroll;
+
+    @BindView(R.id.dismissible) View dismissible;
+    @BindView(R.id.scrollable) TextView scrollable;
 
     public TouchFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         detector = new GestureDetector(context, new GestureListener());
+        scroller = new Scroller(context);
     }
 
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (dismissible == null) {
-            dismissible = findViewById(R.id.dismissible);
-        }
-
+        if (action == null) init();
         return detector.onTouchEvent(event);
+    }
+
+
+    private void init() {
+        maxScroll = scrollable.getLineCount() * scrollable.getLineHeight() - scrollable.getHeight();
+        scrollable.setScroller(scroller);
     }
 
 
@@ -34,6 +50,8 @@ public class TouchFrameLayout extends FrameLayout {
 
         @Override
         public boolean onDown(MotionEvent event) {
+            action = NONE;
+
             float x = event.getX();
             float y = event.getY();
 
@@ -45,11 +63,61 @@ public class TouchFrameLayout extends FrameLayout {
             return left < x && x < right && top < y && y < bottom;
         }
 
+
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            dismissible.setX(dismissible.getX() - distanceX);
-            dismissible.setY(dismissible.getY() - distanceY);
+
+            switch (action) {
+
+                case NONE: {
+                    float dx = Math.abs(distanceX);
+                    float dy = Math.abs(distanceY);
+                    action = (dx > dy) ? DISMISS : SCROLL;
+                    break;
+                }
+
+                case DISMISS: {
+                    dismissible.setX(dismissible.getX() - distanceX);
+                    dismissible.setY(dismissible.getY() - distanceY);
+                    break;
+                }
+
+                case SCROLL: {
+                    scrollable.scrollBy(0, getDy((int) distanceY));
+                    break;
+                }
+            }
+
             return true;
         }
+
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (action != SCROLL) return false;
+
+            int dy = getDy((int) -velocityY / 10);
+            scroller.startScroll(0, scrollable.getScrollY(), 0, dy, 300);
+            scrollable.invalidate();
+
+            return true;
+        }
+
+
+        private int getDy(int distance) {
+            int scroll = scrollable.getScrollY();
+            if (distance < 0) {
+                return (scroll + distance < 0) ? -scroll : distance;
+            } else {
+                return (scroll + distance > maxScroll) ? maxScroll - scroll : distance;
+            }
+        }
+    }
+
+
+    enum Action {
+        NONE,
+        DISMISS,
+        SCROLL
     }
 }
