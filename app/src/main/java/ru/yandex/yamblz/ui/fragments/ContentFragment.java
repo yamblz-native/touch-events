dc91fcf360bb6c73c0064e8c9f5b9b1abfae9f92
Valeriy Shtaihts <bajiepkasmile@yandex.ru>
2016-07-28 00:45:01 +0300

    some commit

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -6,13 +6,20 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import ru.yandex.yamblz.CustomOnTouchListener;
 import ru.yandex.yamblz.R;
 
 public class ContentFragment extends BaseFragment {
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+
+        view.findViewById(R.id.card).setOnTouchListener(new CustomOnTouchListener(view.findViewById(R.id.fl_delete)));
+
+        return view;
     }
+
 }
