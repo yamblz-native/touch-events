3cd3cb1e11fe26dc134aa0a776eca9cb3c346807
DAlexIV <dalexiv@yandex.ru>
2016-07-31 20:00:07 +0300

    Base delete

--- app/src/main/java/ru/yandex/yamblz/ui/views/SwipeableCard.java
+++ app/src/main/java/ru/yandex/yamblz/ui/views/SwipeableCard.java
@@ -1,13 +1,18 @@
 package ru.yandex.yamblz.ui.views;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import ru.yandex.yamblz.R;
@@ -18,21 +23,45 @@ import ru.yandex.yamblz.R;
 
 public class SwipeableCard extends CardView {
     private static final String TAG = SwipeableCard.class.getSimpleName();
+
+    // Animation constants
+    private static final int FADE_OUT_ROTATION = 90;
+    private static final float ROTATION_COEF = 1.0f / 20;
+    private static final int ANIMATION_DURATION = 200;
+    private static final int MAX_RETURN_BACK_ROTATION = 15;
+
+
     private GestureDetectorCompat gd = new GestureDetectorCompat(getContext(), new FlingGestureListener());
 
     public SwipeableCard(Context context) {
         super(context);
-
     }
 
     public SwipeableCard(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    private void init() {
+    private void addImageView() {
         final ImageView child = new ImageView(getContext());
         child.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.anwerrr));
-        addView(child);
+
+        FrameLayout.LayoutParams params
+                = new FrameLayout.LayoutParams(getWidth() / 2, getHeight() / 2,
+                Gravity.CENTER_HORIZONTAL ^ Gravity.CENTER_VERTICAL);
+        child.setImageAlpha(0);
+
+        // TODO place below cardview
+        addView(child, params);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (getChildCount() == 1) {
+            addImageView();
+            requestLayout();
+        }
     }
 
     @Override
@@ -43,26 +72,43 @@ public class SwipeableCard extends CardView {
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         super.dispatchTouchEvent(ev);
-        Log.d(TAG, ev.toString());
+//        Log.d(TAG, ev.toString());
         // Handle scroll
         gd.onTouchEvent(ev);
 
         // Handle ACTION_UP
         if (ev.getAction() == MotionEvent.ACTION_UP) {
-            if (Math.abs(getRotation()) < 30) {
-                Log.d(TAG, "Current rotation is " + getRotation());
+            // Card animation
+            if (Math.abs(getRotation()) < MAX_RETURN_BACK_ROTATION) {
+                // Return card back
+//                Log.d(TAG, "Current rotation is " + getRotation());
                 animate().rotation(0)
                         .translationX(0)
-                        .setStartDelay(100)
-                        .setDuration(100);
+                        .setStartDelay(0)
+                        .setDuration(ANIMATION_DURATION)
+                        .setInterpolator(new FastOutSlowInInterpolator());
             } else {
+                // Swipe card out
                 float translationToGo = getTranslationX() > 0
                         ? getHeight() : -getHeight();
-                animate().rotation(90)
+                float rotationToGo = getRotation() > 0
+                        ? FADE_OUT_ROTATION : -FADE_OUT_ROTATION;
+
+                animate().rotation(rotationToGo)
                         .translationX(translationToGo)
                         .alpha(0)
-                        .setStartDelay(100)
-                        .setDuration(100);
+                        .setStartDelay(0)
+                        .setDuration(ANIMATION_DURATION);
+            }
+
+//             Delete button animation
+            if (getChildCount() > 1) {
+                ImageView viewToAnimate = (ImageView) getChildAt(1);
+                ObjectAnimator animator = ObjectAnimator.ofInt(viewToAnimate,
+                        "alpha", viewToAnimate.getImageAlpha(), 0);
+                animator.setInterpolator(new FastOutSlowInInterpolator());
+                animator.setDuration(ANIMATION_DURATION);
+                animator.start();
             }
         }
 
@@ -80,9 +126,22 @@ public class SwipeableCard extends CardView {
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
             final float delta = e2.getX() - e1.getX();
             animate().translationXBy(delta)
-                    .rotationBy(delta / 10)
+                    .rotationBy(delta * ROTATION_COEF)
                     .setStartDelay(0)
                     .setDuration(0);
+
+            if (getChildCount() > 1) {
+                ImageView viewToAnimate = (ImageView) getChildAt(1);
+                final int alpha = (int) (Math.abs(getRotation())
+                        / (2 * MAX_RETURN_BACK_ROTATION) * 255.0);
+                viewToAnimate.setImageAlpha(alpha);
+
+                viewToAnimate.animate()
+                        .translationXBy(-delta)
+                        .setStartDelay(0)
+                        .setDuration(0);
+            }
+            Log.d(TAG, "" + getRotation());
 
             return true;
         }
